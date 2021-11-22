bccd27a53a341f407d2fac59d120aa36267e304c
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-11-22 03:44:17 -0500

    authentication setup

--- server/passport/setup.js
+++ server/passport/setup.js
@@ -1,18 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-// const { PrismaClient } = require("@prisma/client");
-// const prisma = new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 let googleOptions = {
     callbackURL:
         process.env.NODE_ENV === "production"
-            ? "https://editorprod.herokuapp.com/api/google/redirect"
+            ? "https://codebaseprod.herokuapp.com/api/google/redirect"
             : "/api/google/redirect",
     clientID:
-        process.env.client_id || require("./googlePlus.json").web.client_id,
+        process.env.client_id || require("./googleSetup.json").web.client_id, // google plus
     clientSecret:
         process.env.client_secret ||
-        require("./googlePlus.json").web.client_secret
+        require("./googleSetup.json").web.client_secret
 };
 
 passport.serializeUser((user, done) => {
@@ -26,43 +26,23 @@ passport.deserializeUser((user, done) => {
 passport.use(
     new GoogleStrategy(
         googleOptions,
-        (accessToken, refreshToken, profile, done) => {
-            // const user = await prisma.user.findUnique({
-            //     where: {
-            //         id: profile.id
-            //     }
-            // });
-            // if (user) {
-            //     return done(null, user);
-            // } else {
-            //     const newuser = await prisma.user.create({
-            //         data: {
-            //             id: profile.id,
-            //             email: profile.email,
-            //             name: profile.displayName
-            //         }
-            //     });
-            //     return done(null, newuser);
-            // }
-            // const findQuery = `SELECT * FROM users WHERE displayName = $1`;
-            // const findQueryValues = [profile.displayName];
-            // client.query(findQuery, findQueryValues, (err, foundUser) => {
-            //     foundUser = foundUser.rows[0];
-            //     if (!foundUser) {
-            //         const insertQuery =
-            //             "INSERT INTO users(googleid, displayName) VALUES($1, $2) RETURNING *";
-            //         const insertQueryValues = [profile.id, profile.displayName];
-            //         client.query(
-            //             insertQuery,
-            //             insertQueryValues,
-            //             (err, createdUser) => {
-            //                 done(null, createdUser);
-            //             }
-            //         );
-            //     } else {
-            //         done(null, foundUser);
-            //     }
-            // });
+        async (accessToken, refreshToken, profile, done) => {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: profile.id
+                }
+            });
+            if (user) {
+                return done(null, user);
+            } else {
+                const newuser = await prisma.user.create({
+                    data: {
+                        id: profile.id,
+                        name: profile.displayName
+                    }
+                });
+                return done(null, newuser);
+            }
         }
     )
 );
---
+++ src/features/user/userAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export async function logoutUser() {
+    return await axios
+        .get(`/api/logout`)
+        .then((res) => res.data)
+        .catch((err) => console.error(err));
+}
+
+export async function fetchUser() {
+    return await axios
+        .get(`/api/user`)
+        .then((res) => res.data)
+        .catch((err) => console.error(err));
+}
+
+// test function
+(async function () {
+    return await axios
+        .get("/api/me")
+        .then((result) => {
+            console.log("result.data: ", result.data);
+            return result.data;
+        })
+        .catch((err) => console.error(err));
+})();
