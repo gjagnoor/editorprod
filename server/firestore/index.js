cb2824486ec7815fddc113d1f2928164859cc617
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-11-23 15:10:47 -0500

    update

--- server/firestore/index.js
+++ server/firestore/index.js
@@ -1,20 +1,21 @@
+/* eslint-disable max-len */
 const {initializeApp} = require('firebase-admin/app');
-const serviceAccount = require('../../codebasev1-firebase-adminsdk.json');
 const admin = require('firebase-admin');
 
+const key = process.env.NODE_ENV === 'development' ? require('../../codebasev1-firebase-adminsdk.json') : {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
 initializeApp({
-  credential: admin.credential.cert(serviceAccount || {
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url,
-  }),
+  credential: admin.credential.cert(key),
   databaseURL: 'https://<codebasev1>.firebaseio.com',
 });
 
